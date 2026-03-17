c1afe33c70fc8b91ebda4fb1dec1b477e7341c4e
d11416134 <d11416134@gs.takming.edu.tw>
2026-03-17 15:34:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Class_3_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe,tom,mary;
+
+            joe = new Test();
+            mary = new Test();
+
+            joe.Mid = 80;
+            joe.Final = 90;
+
+            mary.Mid = 60;
+            mary.Final = 66;
+
+            Console.WriteLine("Joe" + joe.GetAvg());
+
+            tom = joe;
+
+
+
+            Console.WriteLine("Tom" +tom.GetAvg());
+
+            joe.SetGrade(99, 100);
+
+            Console.WriteLine("Tom" +tom.GetAvg());
+
+
+
+        }
+    }
+}
